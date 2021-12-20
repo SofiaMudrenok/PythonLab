51c2f0af7befddaae6d0f078aceee7866782b473
Sofia <93678956+SofiaMudrenok@users.noreply.github.com>
2021-12-20 14:08:38 +0200

    lab

--- Lab7/7.3.py
+++ Lab7/7.3.py
@@ -1,13 +1,17 @@
 import random
-a = int (input('a ='))
-b = []
+
+b = int (input('b ='))
+a = []
 n = int (input('Введіть кількість рядків матриці:'))
 m = int (input('Введіть кількість стовбців матриці:'))
+
+
 for i in range(n):
     row = []
     for j in range(m):
         elem = random.randint(1,9)
-        row.append(elem*a)
-        b.append(row)
-for i in range(len(b)):
-    print(b[i])
+        row.append(elem * b)
+    a.append(row)
+
+for i in range(len(a)):
+    print(a[i])
